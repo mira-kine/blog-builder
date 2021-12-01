e474b8e5341d44d28c5f50a11a6c5951795797a7
mira-kine <mirak0419@gmail.com>
2021-12-01 10:50:55 -0800

    passed in props to components, added useStates

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 
 import Preview from '../../components/Preview/Preview';
 import Editor from '../../components/Editor/Editor';
@@ -7,12 +8,17 @@ import './Home.css';
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
+  const [title, setTitle] = useState('title');
+  const [subtitle, setSubtitle] = useState('subtitle');
+  const [font, setFont] = useState('font');
+  const [align, setAlign] = useState('align');
+  const [text, setText] = useState('text');
 
   return (
-    <main>
+    <main className="home">
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
-      <Editor />
+      <Preview {...{ title, subtitle, font, align, text }} />
+      <Editor {...{ title, subtitle, font, align, text }} />
     </main>
   );
 }
